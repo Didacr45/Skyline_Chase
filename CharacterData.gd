a58c408e33317b01811673c69a16246fdefe6718
Didacr45 <rubiodelatorredidac@elpuig.xeill.net>
2026-05-20 09:55:46 +0200

    Borrado

--- CharacterData.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-class_name CharacterData
-
-@export var name: String
-@export var model_scene: PackedScene # Aquí arrastrarás el .tscn del personaje
-@export var health: int
-@export var speed: float
